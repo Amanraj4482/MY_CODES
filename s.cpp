1c154d379f23695352ba610641798d2969abaa4c
AmanCore <aman8804623822@gmail>
2023-10-06 18:36:14 +0530

    this is the message before commiting

--- s.cpp
+++ s.cpp
@@ -1,4 +1,4 @@
-// #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 // using namespace std;
 // #define printarr(x) {cerr<<#x;cerr << " [ "; for (auto i : x) {cerr << i << " ";} cerr << "]"; cerr << "\n";}
 // #define bl_ cerr<<endl;
@@ -191,5 +191,5 @@
 
 // 	root.inOrderPrint();
 
-// 	return 0;
+// return 0;
 // }
